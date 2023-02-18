43790ea8e6346ef98e8c320e1623a5b1619e2160
MariuszWiacek <wiacekm.uber@gmail.com>
2023-02-18 21:14:54 +0000

    generate README and markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,35 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
+function generateMarkdown(data) {
+  return `
+  ###<p align="center">**${data.title}**</p>
+  ## Table of Contents
+  * [Description](#description)
+  * [Usage](#usage)
+  * [Languages](#languages)
+  * [Installation Instructions](#installation)
+  * [Collaboratores](#Collaboratores)
+  * [License](#license)
+  * [ContactMe](#contactMe)
+  ## Description
+  * ${data.description}
+  ## Usage
+  * ${data.usage}
+  ## Languages
+  * ${data.languages}
+  ## Installation
+  * ${data.installation}
+  ## Contributing
+  * ${data.collabInput}
+  # License
+  
+  ## Contact me
+  * Profile: https://github.com/${data.userName}
+  * Email: ${data.email} `;
+  
+  
+  
+  
+  }
 
 module.exports = generateMarkdown;
