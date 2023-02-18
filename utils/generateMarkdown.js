03a3b5862aa799fea55ec1a7dccfb2a3946877b1
MariuszWiacek <wiacekm.uber@gmail.com>
2023-02-18 21:24:58 +0000

    changes to generator

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 
 function generateMarkdown(data) {
   return `
-  ### <p align="center">**${data.title}**</p>
+  # <p align="center">${data.title}</p>
   ## Table of Contents
   * [Description](#description)
   * [Usage](#usage)
@@ -21,24 +21,25 @@ function generateMarkdown(data) {
   * ${data.installation}
   ## Contributing
   * ${data.collabInput}
-  # License
-  
+  ## License
+  ${getLicenseBadge(data.license)} 
+  * Link: ${getLicenseLink(data.license)}
   ## Contact me
   * Profile: https://github.com/${data.userName}
   * Email: ${data.email} `;
-  
-  
-  
-  
-  }
 
-  //function that returns a license badge
+
+
+
+}
+
+//function that returns a license badge
 function getLicenseBadge(license) {
-  switch(license){
+  switch (license) {
     case 'MIT':
       return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
       break;
-      case 'Apache 2.0':
+    case 'Apache 2.0':
       return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
       break;
     case 'ISC':
@@ -52,14 +53,14 @@ function getLicenseBadge(license) {
 
 //funtion to return license link
 function getLicenseLink(license) {
-  switch(license){
+  switch (license) {
     case 'MIT':
       return `https://opensource.org/licenses/MIT`;
       break;
-      case 'Apache 2.0':
-        return `https://opensource.org/licenses/Apache-2.0`;
-        break;
-     case 'ISC':
+    case 'Apache 2.0':
+      return `https://opensource.org/licenses/Apache-2.0`;
+      break;
+    case 'ISC':
       return `https://opensource.org/licenses/ISC`;
       break;
     default:
