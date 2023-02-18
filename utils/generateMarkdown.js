ac9e1de89194384b89bf5cca6b93991da510c196
MariuszWiacek <wiacekm.uber@gmail.com>
2023-02-18 21:20:00 +0000

    licence badge and link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 
 function generateMarkdown(data) {
   return `
-  ###<p align="center">**${data.title}**</p>
+  ### <p align="center">**${data.title}**</p>
   ## Table of Contents
   * [Description](#description)
   * [Usage](#usage)
@@ -31,5 +31,41 @@ function generateMarkdown(data) {
   
   
   }
+
+  //function that returns a license badge
+function getLicenseBadge(license) {
+  switch(license){
+    case 'MIT':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+      case 'Apache 2.0':
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case 'ISC':
+      return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+      break;
+    default:
+      return '';
+      break;
+  }
+}
+
+//funtion to return license link
+function getLicenseLink(license) {
+  switch(license){
+    case 'MIT':
+      return `https://opensource.org/licenses/MIT`;
+      break;
+      case 'Apache 2.0':
+        return `https://opensource.org/licenses/Apache-2.0`;
+        break;
+     case 'ISC':
+      return `https://opensource.org/licenses/ISC`;
+      break;
+    default:
+      return '';
+      break;
+  }
+}
 
 module.exports = generateMarkdown;
